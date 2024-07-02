f1316166d15011469377dcf63a4d36363c848039
Kaivalya192005 <kaivalyaagarkar@gmail.com>
2024-07-02 23:50:20 +0530

    Add files via upload

--- greatest number.py
+++ greatest number.py
@@ -1,10 +1,8 @@
 a=int(input("enter the number : "))
 b=int(input("enterthe second number : "))
-c=int(input("enter the third number : "))
+
  
-if(a>=b and a>=c):
-    print("The a is greater than b and c")
-elif(b>=a and b>=c ):
-    print("The b is greater than a and c")
-elif(c>=a and c>=b):
-    print("The c is greater than b and c")
+if(a>=b ):
+    print("The a is greater than b ")
+elif(b>=a ):
+    print("The b is greater than a ")
