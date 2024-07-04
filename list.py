ffa83b6b46b0b32a4a2ff9ca3d307e4c6906b167
Kaivalya192005 <kaivalyaagarkar@gmail.com>
2024-07-04 17:13:18 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,36 @@
+"""marks=[94.4,83.4,96.4]
+#print(marks)
+#print(type(marks))
+#print(marks[0])
+#print(marks[2])
+print(marks[:2])"""
+"""movies=[]
+mov1=input("Enter the first movie :")
+mov2=input("Enter the second movie :")
+mov3=input("Enter the third movie :")
+ 
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)"""
+num=int(input("Enter the number : "))  
+if(num==2):
+    print("2,4,6,8,10,12,14,16,18,20")
+elif(num==3):
+    print("3,6,9,12,15,18,21,24,27,30")
+elif(num==4):
+    print("4,8,12,16,20,24,28,32,36")
+elif(num==5):
+    print("5,10,15,20,25,30,35,40,45,50")
+elif(num==6):
+    print("6,12,18,24,30,36,42,48,54,60")
+elif(num==7):
+    print("7,14,21,28,35,42,49,56,63,70")
+elif(num==8):
+    print("8,16,24,32,40,48,56,64,72,80")
+elif(num==9):
+    print("9,18,27,36,45,54,63,72,81,90")
+elif(num==10):
+    print("10,20,30,40,50,60,70,80,90,100")
+else:
+    print("Invalid number")
